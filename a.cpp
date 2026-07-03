2d1d2edff3d688309140cb427b94746afcb271ac
yuetai <248285823@qq.com>
2026-07-03 13:32:40 +0800

    实现冒泡

--- a.cpp
+++ a.cpp
@@ -3,29 +3,14 @@
 #include"lqueue.h"
 #include"erfen.h"
 #include"diguierfen.h"
+#include"sort.h"
 using namespace std;
-
-
-
-void maopao(int a[],int n)
-{
- for(int i=0;i<n-1;i++)
- {
-  for(int j=0;j<n-1;j++)
-   {
-    if(a[j]>a[j+1])swap(a[j],a[j+1]);
-   }
-
-
- }
-}
-
 
 
 int main()
 {
 int a[]={5,46,73,3,5764,34,2,775};
-maopao(a,8);
+bubblesort(a,8);
 for(int i=0;i<8;i++)
 {
     cout<<a[i]<<" ";
---
+++ sort.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"sort.h"
+using namespace std;
+
+void bubblesort(int a[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])swap(a[j],a[j+1]);
+        }
+    }
+}
